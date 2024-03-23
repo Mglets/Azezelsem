cee08dd3913cccd510a25f53ca55e3d4d40f1fb4
Mo3azsahaheen <164342152+Mglets@users.noreply.github.com>
2024-03-23 02:52:20 +0200

    Create azazelSEm.py

---
+++ azazelSEm.py
@@ -0,0 +1,11 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def display_trimix_image():
+    # Load the Trimix image
+    trimix_image = mpimg.imread( trimix.png )
+
+    # Display the image
+    plt.imshow(trimix_image)
+    plt.show()
