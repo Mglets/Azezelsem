b192528a02dedbc5ce86a72931bfabeb3c7977b2
Mo3azsahaheen <164342152+Mglets@users.noreply.github.com>
2024-03-23 02:55:36 +0200

    Update azazelSEm.py

--- azazelSEm.py
+++ azazelSEm.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 
 def display_trimix_image():
     # Load the Trimix image
-    trimix_image = mpimg.imread( trimix.png )
+    trimix_image = mpimg.imread( 0102.jpeg)
 
     # Display the image
     plt.imshow(trimix_image)
