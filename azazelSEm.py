fbed531621bec37fd067cd4159f548ec5387e90a
Mo3azsahaheen <164342152+Mglets@users.noreply.github.com>
2024-03-23 03:02:19 +0200

    Update azazelSEm.py

--- azazelSEm.py
+++ azazelSEm.py
@@ -1,11 +1,10 @@
-
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-def display_trimix_image():
-    # Load the Trimix image
-    trimix_image = mpimg.imread( 0102.jpeg)
+def display_azazel_sem_image():
+    # Load the Azazel SEM image
+    azazel_sem_image = mpimg.imread( 'OIG2.jpeg' )
 
     # Display the image
-    plt.imshow(trimix_image)
+    plt.imshow(azazel_sem_image)
     plt.show()
